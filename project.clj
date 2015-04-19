2c2e84bbdeea56e83139fd288a54c5ff857c42f5
Brian Marick <marick@exampler.com>
2015-04-19 17:47:15 -0500

    compatibility testing

---
+++ project.clj
@@ -0,0 +1,24 @@
+(defproject midje-comparisons "0.1.0"
+  :description "Comparisons as used by Midje."
+  :url "https://github.com/marick/Midje"
+  ;; :pedantic? :warn
+  :license {:name "The MIT License (MIT)"
+            :url "http://opensource.org/licenses/mit-license.php"
+            :distribution :repo}
+  :mailing-list {:name "Midje"
+                 :subscribe "https://groups.google.com/forum/?fromgroups#!forum/midje"}
+
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:dev {:dependencies [[midje "1.7.0-beta1"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5.0 {:dependencies [[org.clojure/clojure "1.5.0"]]}
+             :1.5.1 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-beta1"]]}
+             }
+
+  ;; For Clojure snapshots
+  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
+)
+
+  
