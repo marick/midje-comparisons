f4891e9cc9ed938d89a50323eb75d433c4c3a8ca
Brian Marick <marick@exampler.com>
2015-04-21 10:55:49 -0500

    Add Travis

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # midje-comparisons
 
 The comparison functions used by [Midje](https://github.com/marick/Midje/).
+
+[![Build Status](https://travis-ci.org/marick/midje-comparisons.png?branch=master)](https://travis-ci.org/marick/midje-comparisons)
--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject midje-comparisons "0.1.0"
+(defproject midje/midje-comparisons "0.1.0"
   :description "Comparisons as used by Midje."
   :url "https://github.com/marick/Midje"
-  ;; :pedantic? :warn
+  :pedantic? :warn
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/mit-license.php"
             :distribution :repo}
@@ -9,8 +9,8 @@
                  :subscribe "https://groups.google.com/forum/?fromgroups#!forum/midje"}
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [marick/clojure-commons "0.3.2"]]
-  :profiles {:dev {:dependencies [[midje "1.7.0-beta1"]]}
+                 [marick/clojure-commons "0.4.0" :exclusions [org.clojure/clojure]]]
+  :profiles {:dev {:dependencies [[midje "2.0.0-SNAPSHOT" :exclusions [org.clojure/clojure]]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5.0 {:dependencies [[org.clojure/clojure "1.5.0"]]}
              :1.5.1 {:dependencies [[org.clojure/clojure "1.5.1"]]}
@@ -18,8 +18,10 @@
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0-beta1"]]}
              }
 
+  :aliases {"compatibility" ["with-profile" "+1.4:+1.5.0:+1.5.1:+1.6:+1.7" "midje" ":config" ".compatibility-test-config"]
+            "travis" ["with-profile" "+1.4:+1.5.0:+1.5.1:+1.6:+1.7" "midje"]}
+
+
   ;; For Clojure snapshots
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
 )
-
-  
